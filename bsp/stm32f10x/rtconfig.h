e4fe303459eecb41ff6722e4cd958aed5bf0191e
Lighter-z <1106606588@qq.com>
2020-07-27 21:07:49 +0800

    1、修改 SConstruct 中的 RTT_ROOT 路径位置 2、使用命令新建mdk5工程

--- bsp/stm32f10x/rtconfig.h
+++ bsp/stm32f10x/rtconfig.h
@@ -39,8 +39,9 @@
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart1"
-#define RT_VER_NUM 0x40000
+#define RT_VER_NUM 0x40003
 #define ARCH_ARM
+#define RT_USING_CPU_FFS
 #define ARCH_ARM_CORTEX_M
 #define ARCH_ARM_CORTEX_M3
 
@@ -79,26 +80,25 @@
 #define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
 #define RT_SERIAL_USING_DMA
+#define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
-
-/* Using WiFi */
-
 
 /* Using USB */
 
 
 /* POSIX layer and C standard library */
 
+#define RT_LIBC_USING_TIME
 
 /* Network */
 
 /* Socket abstraction layer */
 
 
+/* Network interface device */
+
+
 /* light weight TCP/IP stack */
-
-
-/* Modbus master and slave stack */
 
 
 /* AT commands */
@@ -108,9 +108,6 @@
 
 
 /* Utilities */
-
-
-/* ARM CMSIS */
 
 
 /* RT-Thread online packages */
@@ -157,9 +154,11 @@
 
 /* Hardware Drivers Config */
 
-#define SOC_STM32F103RB
+#define SOC_STM32F103ZE
 
 /* Onboard Peripheral Drivers */
+
+#define BSP_USING_USB_TO_USART
 
 /* On-chip Peripheral Drivers */
 
